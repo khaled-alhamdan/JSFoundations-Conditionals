c85976ee29f522824fb0c5027c1b4b3d2c1b32d6
Hamdani <78157427+khaled-alhamdan@users.noreply.github.com>
2021-02-02 19:59:06 +0300

    Task 4 completed

--- calculator.js
+++ calculator.js
@@ -1,3 +1,27 @@
 // Define the numbers and the operation here
+let firstNumber = 3;
+let secondNumber = 3;
+let operation = "*";
 
 // Write if-statements here
+if (operation === "+") {
+  console.log("its addition");
+  console.log(
+    `${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`
+  );
+} else if (operation === "-") {
+  console.log("its subtraction");
+  console.log(
+    `${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`
+  );
+} else if (operation === "*") {
+  console.log("its multiplication");
+  console.log(
+    `${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`
+  );
+} else {
+  console.log("its division");
+  console.log(
+    `${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`
+  );
+}
